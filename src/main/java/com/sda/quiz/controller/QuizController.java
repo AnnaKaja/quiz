e915d88f10a43e6a2a128cc7595b68c5a1997995
anna kaja <anna_kaja@op.pl>
2018-07-24 18:31:17 +0200

    next question mechanism

--- src/main/java/com/sda/quiz/controller/QuizController.java
+++ src/main/java/com/sda/quiz/controller/QuizController.java
@@ -1,5 +1,6 @@
 package com.sda.quiz.controller;
 
+import com.sda.quiz.domain.Question;
 import com.sda.quiz.service.QuestionService;
 import com.sda.quiz.service.QuizService;
 import org.springframework.stereotype.Controller;
@@ -19,24 +20,29 @@ public class QuizController {
         this.questionService = questionService;
     }
 
-    @GetMapping ("/quiz")
-    public ModelAndView getAll(){
+    @GetMapping("/quiz")
+    public ModelAndView getAll() {
         ModelAndView modelAndView = new ModelAndView();
         modelAndView.setViewName("allQuiz");
-
         return modelAndView;
     }
+
     @GetMapping("/quiz/{id}")
     public ModelAndView getQuiz(@PathVariable("id") long id) {
         ModelAndView modelAndView = new ModelAndView("quiz");
         modelAndView.addObject("quiz", quizService.getQuiz(id));
         return modelAndView;
     }
-    @GetMapping("/quiz/{id}/question/{questionId}")
-    public ModelAndView getQuestion(@PathVariable("questionId") long id){
+
+    @GetMapping("/quiz/{quizId}/question/{questionId}")
+    public ModelAndView getQuestion(@PathVariable("questionId") long questionId, @PathVariable("quizId") long quizId) {
         ModelAndView modelAndView = new ModelAndView("quizQuestion");
-        modelAndView.addObject("question", questionService.getQuestion(id));
-        return  modelAndView;
+        modelAndView.addObject("question", questionService.getQuestion(questionId));
+        modelAndView.addObject("nextQuestionPath", fetchNextQuestionPath(questionService.fetchNextQuestion(quizId, questionId), quizId));
+        return modelAndView;
     }
 
+    private String fetchNextQuestionPath(Question question, long quizId) {
+        return question == null ? null : "/quiz/" + quizId + "/question/" + question.getId();
+    }
 }
